ffcad537d7b93a8d37f9d37f7ad3bd5a5db25c1e
Levminer <33373714+Levminer@users.noreply.github.com>
2020-08-04 19:58:42 +0200

    NS DEV 2.2.0

--- express/server.js
+++ express/server.js
@@ -1,4 +1,6 @@
-const express = require('express')
+const express = require("express")
+const mongoose = require("mongoose")
+const Model = require("./Model")
 
 const app = express()
 const port = 8080
@@ -11,24 +13,87 @@ app.locals.server = server
 app.locals.node = node
 app.locals.version = version
 
-app.set('view engine', 'ejs');
+//statistics
+let stat
 
-app.get('/', (req, res) => {
-    res.render('index', {
-    })
-    app.use(express.static(__dirname + '/views'));  
+// DB
+const db =
+  "mongodb+srv://ser:1234@mnr-fyqdo.mongodb.net/test?retryWrites=true&w=majority"
+mongoose.connect(db, (err) => {
+  if (err) return console.log("ERROR")
+  console.log("MDB connected!")
 })
 
-app.get('/en', (req, res) => {
-    res.render('en', {
-    })
-    app.use(express.static(__dirname + '/views'));  
+app.set("view engine", "ejs")
+app.use(express.json({ limit: "1mb" }))
+
+app.get("/", (req, res) => {
+  res.render("index", {})
+  app.use(express.static(__dirname + "/views"))
 })
 
-app.get('/hu', (req, res) => {
-    res.render('hu', {
-    })
-    app.use(express.static(__dirname + '/views'));  
+app.get("/en", (req, res) => {
+  res.render("en", {})
+  app.use(express.static(__dirname + "/views"))
+})
+
+app.get("/hu", (req, res) => {
+  res.render("hu", {})
+  app.use(express.static(__dirname + "/views"))
+})
+
+app.post("/api", (req, res) => {
+  console.log("Data coming:")
+  stat = req.body
+
+  Model.findOneAndUpdate(
+    { _id: "5f296c398ba8042f10da6238" },
+    { $push: { saved_statistics: stat } },
+    function (error, success) {
+      if (error) {
+        console.log(error)
+      } else {
+        console.log(success)
+      }
+    }
+  )
+
+  res.end()
+})
+
+app.get("/api", (req, res) => {
+  console.log("Seraching data...")
+
+  Model.find({}).exec((err, users) => {
+    if (err) {
+      console.log("Searh error")
+    } else {
+      res.json(users)
+      console.log(users)
+    }
+  })
+})
+
+app.get("/ins", (req, res) => {
+  var objFriends = "ASDASDASDASD"
+  Model.findOneAndUpdate(
+    { _id: "5f296c398ba8042f10da6238" },
+    { $push: { saved_statistics: objFriends } },
+    function (error, success) {
+      if (error) {
+        res.send("Error")
+        console.log(error)
+      } else {
+        res.send("Succes")
+        console.log(success)
+      }
+    }
+  )
+})
+
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!")
+  app.use(express.static(__dirname + "/views"))
 })
 
 app.listen(port)
--- express/views/assets/en/js/index.js
+++ express/views/assets/en/js/index.js
@@ -1,117 +1,130 @@
 let restart = () => {
-	//?reload the page
-	location.reload()
+  //?reload the page
+  location.reload()
 }
 
 let start = () => {
-	//?make the names and values
-	let titles = []
-	let results = {}
+  //?make the names and values
+  let titles = []
+  let results = {}
 
-	// make the titles
-	for (let i = 0; i < data.length; i++) {
-		let split = data[i].split(":")
-		titles.push(split[0])
-	}
+  // make the titles
+  for (let i = 0; i < data.length; i++) {
+    let split = data[i].split(":")
+    titles.push(split[0])
+  }
 
-	// counting titles
-	titles.forEach((x) => {
-		results[x] = (results[x] || 0) + 1
-	})
+  // counting titles
+  titles.forEach((x) => {
+    results[x] = (results[x] || 0) + 1
+  })
 
-	//separate names and values
-	let chart_data_1 = Object.keys(results)
-	let chart_data_2 = Object.values(results)
+  //send results to API
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(results),
+  }
 
-	console.log("Chart names:")
-	console.log(chart_data_1)
-	console.log("Chart values:")
-	console.log(chart_data_2)
+  fetch("/api", options).then((res) => {
+    console.log(res)
+  })
 
-	//? make the statistics
-	let title = chart_data_1.length
+  //separate names and values
+  let chart_data_1 = Object.keys(results)
+  let chart_data_2 = Object.values(results)
 
-	// calculate statitics
-	let watchtime_minute = Math.floor(data.length * 50)
-	let watchtime_hour = Math.floor(watchtime_minute / 60)
-	let watchtime_day = Math.floor(watchtime_hour / 24)
+  console.log("Chart names:")
+  console.log(chart_data_1)
+  console.log("Chart values:")
+  console.log(chart_data_2)
 
-	console.log(`Title: ${title}`)
-	console.log(`Watchtime_m: ${watchtime_minute}`)
-	console.log(`Watchtime_h: ${watchtime_hour}`)
-	console.log(`Watchtime_d: ${watchtime_day}`)
+  //? make the statistics
+  let title = chart_data_1.length
 
-	//animated counters
-	let c1 = new CountUp("h31", 0, title)
-	let c2 = new CountUp("h32", 0, watchtime_minute)
-	let c3 = new CountUp("h33", 0, watchtime_hour)
-	let c4 = new CountUp("h34", 0, watchtime_day)
-	c1.start()
-	c2.start()
-	c3.start()
-	c4.start()
+  // calculate statitics
+  let watchtime_minute = Math.floor(data.length * 50)
+  let watchtime_hour = Math.floor(watchtime_minute / 60)
+  let watchtime_day = Math.floor(watchtime_hour / 24)
 
-	//set the elements
-	let foo = document.querySelector("#foo")
-	let two = document.querySelector(".two")
-	let but = document.querySelector("#but")
-	foo.style.bottom = "-90px"
-	two.style.display = "block"
-	but.style.display = "inline"
+  console.log(`Title: ${title}`)
+  console.log(`Watchtime_m: ${watchtime_minute}`)
+  console.log(`Watchtime_h: ${watchtime_hour}`)
+  console.log(`Watchtime_d: ${watchtime_day}`)
 
-	//? make random chart colors
-	let colors = []
+  //animated counters
+  let c1 = new CountUp("h31", 0, title)
+  let c2 = new CountUp("h32", 0, watchtime_minute)
+  let c3 = new CountUp("h33", 0, watchtime_hour)
+  let c4 = new CountUp("h34", 0, watchtime_day)
+  c1.start()
+  c2.start()
+  c3.start()
+  c4.start()
 
-	let rgb_start = () => {
-		for (let i = 0; i < chart_data_1.length; i++) {
-			let rgb_color0 = ""
-			let rgb_color1 = ""
-			let rgb_color2 = ""
+  //set the elements
+  let foo = document.querySelector("#foo")
+  let two = document.querySelector(".two")
+  let but = document.querySelector("#but")
+  foo.style.bottom = "-90px"
+  two.style.display = "block"
+  but.style.display = "inline"
 
-			for (let i = 0; i < 3; i++) {
-				let rgb_randomizer = Math.floor(Math.random() * 256)
+  //? make random chart colors
+  let colors = []
 
-				switch (i) {
-					case 0:
-						rgb_color0 += rgb_randomizer
-						break
-					case 1:
-						rgb_color1 += rgb_randomizer
-						break
-					case 2:
-						rgb_color2 += rgb_randomizer
-						break
-				}
-			}
+  let rgb_start = () => {
+    for (let i = 0; i < chart_data_1.length; i++) {
+      let rgb_color0 = ""
+      let rgb_color1 = ""
+      let rgb_color2 = ""
 
-			let rgb = `rgb(${rgb_color0},${rgb_color1},${rgb_color2})`
+      for (let i = 0; i < 3; i++) {
+        let rgb_randomizer = Math.floor(Math.random() * 256)
 
-			colors.push(rgb)
-		}
-	}
+        switch (i) {
+          case 0:
+            rgb_color0 += rgb_randomizer
+            break
+          case 1:
+            rgb_color1 += rgb_randomizer
+            break
+          case 2:
+            rgb_color2 += rgb_randomizer
+            break
+        }
+      }
 
-	rgb_start()
+      let rgb = `rgb(${rgb_color0},${rgb_color1},${rgb_color2})`
 
-	console.log("Colors:")
-	console.log(colors)
+      colors.push(rgb)
+    }
+  }
 
-	//? Make the chart
-	let mychart = document.getElementById("chart").getContext("2d")
+  rgb_start()
 
-	Chart.defaults.global.defaultFontColor = "white"
+  console.log("Colors:")
+  console.log(colors)
 
-	let thechart = new Chart(mychart, {
-		type: "pie",
-		data: {
-			labels: chart_data_1,
-			datasets: [
-				{
-					label: "numbers",
-					data: chart_data_2,
-					backgroundColor: colors,
-					borderWidth: 3,
-				},
-			],
-		},
-	})
+  //? Make the chart
+  let mychart = document.getElementById("chart").getContext("2d")
+
+  Chart.defaults.global.defaultFontColor = "white"
+
+  let thechart = new Chart(mychart, {
+    type: "pie",
+    data: {
+      labels: chart_data_1,
+      datasets: [
+        {
+          label: "numbers",
+          data: chart_data_2,
+          backgroundColor: colors,
+          borderWidth: 3,
+        },
+      ],
+    },
+  })
 }
