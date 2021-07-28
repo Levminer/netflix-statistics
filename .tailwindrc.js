b80390314f7213fd78a9d5c0acaa7a6ac5ea0b46
Levminer <levminer@levminer.com>
2021-07-28 11:16:51 +0000

    setup tailwind

---
+++ .tailwindrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+	mode: "jit",
+	purge: {
+		enabled: true,
+		content: ["index.html"],
+	},
+	theme: {
+		extend: {
+			inset: {
+				"-30": "-1.875rem",
+			},
+
+			colors: {
+				gray: {
+					500: "#282828",
+					600: "#1E1E1E",
+					700: "#141414",
+					800: "#0a0a0a",
+					900: "#000000",
+				},
+				popup: {
+					red: "#FF0000",
+					green: "#008000",
+					blue: "#0000FF",
+				},
+				html: {
+					gray: "#808080",
+				},
+				ns: {
+					red: "#de0a0a"
+				}
+			},
+
+			fontFamily: {
+				sans: ["Arial", "Helvetica", "sans-serif"],
+			},
+
+			borderRadius: {
+				"2xl": "30px",
+			},
+		},
+	},
+}
